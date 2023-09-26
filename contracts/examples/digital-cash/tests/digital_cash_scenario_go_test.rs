a4723122e548e45997360c664a37dd3ab315a140
Mother Earth <battulaanil36@gmail.com>
2023-09-26 16:12:29 +0530

    0.03

---
+++ contracts/examples/digital-cash/tests/digital_cash_scenario_go_test.rs
@@ -0,0 +1,55 @@
+use dharitri_sc_scenario::*;
+
+fn world() -> ScenarioWorld {
+    ScenarioWorld::vm_go()
+}
+
+#[test]
+fn claim_moa_go() {
+    world().run("scenarios/claim-moa.scen.json");
+}
+
+#[test]
+fn claim_dct_go() {
+    world().run("scenarios/claim-dct.scen.json");
+}
+
+#[test]
+fn claim_fees_go() {
+    world().run("scenarios/claim-fees.scen.json");
+}
+
+#[test]
+fn claim_multi_dct_go() {
+    world().run("scenarios/claim-multi-dct.scen.json");
+}
+
+#[test]
+fn forward_go() {
+    world().run("scenarios/forward.scen.json");
+}
+
+#[test]
+fn fund_moa_and_dct_go() {
+    world().run("scenarios/fund-moa-and-dct.scen.json");
+}
+
+#[test]
+fn set_accounts_go() {
+    world().run("scenarios/set-accounts.scen.json");
+}
+
+#[test]
+fn withdraw_moa_go() {
+    world().run("scenarios/withdraw-moa.scen.json");
+}
+
+#[test]
+fn withdraw_dct_go() {
+    world().run("scenarios/withdraw-dct.scen.json");
+}
+
+#[test]
+fn withdraw_multi_dct_go() {
+    world().run("scenarios/withdraw-multi-dct.scen.json");
+}
---
+++ framework/base/src/storage/protected_keys.rs
@@ -0,0 +1,2 @@
+pub static DHARITRIX_REWARD_KEY: &[u8] = b"DHARITRIXreward";
+pub static DHARITRIX_DCT_LOCAL_ROLES_KEY: &[u8] = b"DHARITRIXroledct";
